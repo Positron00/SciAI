f345e4f691556a38df6ef85d9f22573dcf8a1c36
Positron00 <p051tr0n00000@gmail.com>
2024-11-14 20:38:47 -0500

    add model class and @app.get and .post

--- llm_serve.py
+++ llm_serve.py
@@ -9,3 +9,24 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import StreamingResponse
 load_dotenv('.env', override=True)
 
+# Assign an instance of the FastAPI class to the variable "app".
+# You will interact with your api using this instance.
+app = FastAPI(title='Deploying an ML Model with FastAPI')
+
+# List available models using Enum for convenience. This is useful when the options are pre-defined.
+class Model(str, Enum):
+    yolov3tiny = "yolov3-tiny"
+    yolov3 = "yolov3"
+
+
+# By using @app.get("/") you are allowing the GET method to work for the / endpoint.
+@app.get("/")
+def home():
+    return "Congratulations! Your API is working as expected. Now head over to http://serve/docs"
+
+
+# This endpoint handles all the logic necessary for the object detection to work.
+# It requires the desired model and the image in which to perform object detection.
+@app.post("/predict") 
+def predict(model: Model, file: UploadFile = File(...)):
+    pass
