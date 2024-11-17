469878678e8ab895101e6d6d21f823413a710f0a
Positron00 <p051tr0n00000@gmail.com>
2024-11-16 21:47:55 -0500

    paste code for object recogniton through a server

--- nbs/llm_serve.py
+++ nbs/llm_serve.py
@@ -28,8 +28,48 @@ def home():
 # This endpoint handles all the logic necessary for the object detection to work.
 # It requires the desired model and the image in which to perform object detection.
 @app.post("/predict") 
-def predict(model: Model, file: UploadFile = File(...)):
-    pass
+def prediction(model: Model, file: UploadFile = File(...)):
+
+    # 1. VALIDATE INPUT FILE
+    filename = file.filename
+    fileExtension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not fileExtension:
+        raise HTTPException(status_code=415, detail="Unsupported file provided.")
+    
+    # 2. TRANSFORM RAW IMAGE INTO CV2 image
+    
+    # Read image as a stream of bytes
+    image_stream = io.BytesIO(file.file.read())
+    
+    # Start the stream from the beginning (position zero)
+    image_stream.seek(0)
+    
+    # Write the stream of bytes into a numpy array
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    
+    # Decode the numpy array as an image
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    
+    # 3. RUN OBJECT DETECTION MODEL
+    
+    # Run object detection
+    bbox, label, conf = cv.detect_common_objects(image, model=model)
+    
+    # Create image that includes bounding boxes and labels
+    output_image = draw_bbox(image, bbox, label, conf)
+    
+    # Save it in a folder within the server
+    cv2.imwrite(f'images_uploaded/{filename}', output_image)
+    
+    
+    # 4. STREAM THE RESPONSE BACK TO THE CLIENT
+    
+    # Open the saved image for reading in binary mode
+    file_image = open(f'images_uploaded/{filename}', mode="rb")
+    
+    # Return the image as a stream specifying media type
+    return StreamingResponse(file_image, media_type="image/jpeg")
 
 if __name__ == "__main__":
     # Allows the server to be run in this interactive environment
