128f68f9cc3f7815d6bd51ccddbb70304d7e872d
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-11-17 20:56:15 -0500

    add client function

--- nbs/llm_serve.py
+++ nbs/llm_serve.py
@@ -80,3 +80,26 @@ if __name__ == "__main__":
 
     # Spin up the server!    
     uvicorn.run(app, host=host, port=8000, root_path="/serve")
+
+
+# code a minimal client to interact with the server
+
+def response_from_server(url, image_file, verbose=True):
+    """Makes a POST request to the server and returns the response.
+
+    Args:
+        url (str): URL that the request is sent to.
+        image_file (_io.BufferedReader): File to upload, should be an image.
+        verbose (bool): True if the status of the response should be printed. False otherwise.
+
+    Returns:
+        requests.models.Response: Response from the server.
+    """
+    
+    files = {'file': image_file}
+    response = requests.post(url, files=files)
+    status_code = response.status_code
+    if verbose:
+        msg = "Everything went well!" if status_code == 200 else "There was an error when handling the request."
+        print(msg)
+    return response
