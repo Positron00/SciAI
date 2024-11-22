990760f593a17b2ef33460bcd288b308349c0b5b
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-11-21 19:16:13 -0500

    add example def llama32()

--- nbs/llm_QA.py
+++ nbs/llm_QA.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import json
 
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -68,3 +70,26 @@ class LLM:
         except TypeError as e:
             print("Error:", str(e))
 
+# an example function for using Llama3.2, to be integrated into the LLM class
+def llama32(messages, model_size=11):
+  model = f"meta-llama/Llama-3.2-{model_size}B-Vision-Instruct-Turbo"
+  url = "https://api.together.xyz/v1/chat/completions"
+  payload = {
+    "model": model,
+    "max_tokens": 4096,
+    "temperature": 0.0,
+    "stop": ["<|eot_id|>","<|eom_id|>"],
+    "messages": messages
+  }
+
+  headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + os.environ["TOGETHER_API_KEY"]
+  }
+  res = json.loads(requests.request("POST", url, headers=headers, data=json.dumps(payload)).content)
+
+  if 'error' in res:
+    raise Exception(res['error'])
+
+  return res['choices'][0]['message']['content']
