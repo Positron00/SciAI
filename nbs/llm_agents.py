a6a86526ab84f71d16eee7fc2c287017247eecdb
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-11-21 19:37:22 -0500

    add Agent class

--- nbs/llm_agents.py
+++ nbs/llm_agents.py
@@ -36,3 +36,66 @@ async def run_main():
 #await run_main()
 
 
+import asyncio
+from typing import List, Optional, Dict
+
+from llama_stack_client import LlamaStackClient
+from llama_stack_client.lib.agents.event_logger import EventLogger
+
+from llama_stack_client.types import SamplingParams, UserMessage
+from llama_stack_client.types.agent_create_params import AgentConfig
+
+class Agent:
+    def __init__(self):
+        self.client = LlamaStackClient(
+            base_url=LLAMA_STACK_API_TOGETHER_URL,
+        )
+
+    def create_agent(self, agent_config: AgentConfig):
+        agent = self.client.agents.create(
+            agent_config=agent_config,
+        )
+        self.agent_id = agent.agent_id
+        session = self.client.agents.sessions.create(
+            agent_id=agent.agent_id,
+            session_name="example_session",
+        )
+        self.session_id = session.session_id
+
+    async def execute_turn(self, content: str):
+        response = self.client.agents.turns.create(
+            agent_id=self.agent_id,
+            session_id=self.session_id,
+            messages=[
+                UserMessage(content=content, role="user"),
+            ],
+            stream=True,
+        )
+
+        for chunk in response:
+            if chunk.event.payload.event_type != "turn_complete":
+                yield chunk
+
+async def run_main():
+    agent_config = AgentConfig(
+        model=LLAMA31_8B_INSTRUCT,
+        instructions="You are a helpful assistant",
+        enable_session_persistence=False,
+    )
+
+    agent = Agent()
+    agent.create_agent(agent_config)
+
+    prompts = [
+        "What is the world's largest living structure, according to the Guinness World Records?",
+        "How large is it?",
+    ]
+
+    for prompt in prompts:
+        print(f"User> {prompt}")
+        response = agent.execute_turn(content=prompt)
+        async for log in EventLogger().log(response):
+            if log is not None:
+                log.print()
+
+#await run_main()
