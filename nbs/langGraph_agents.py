d1253d4bcd0ecffa16fda54083cdb8787a612777
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-12-14 13:38:55 -0500

    add persistent memory and streaming responses

--- nbs/langGraph_agents.py
+++ nbs/langGraph_agents.py
@@ -128,27 +128,22 @@ tool = TavilySearchResults(max_results=4) #increased number of results
 class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
 
-class Agent:
+from langgraph.checkpoint.sqlite import SqliteSaver
 
-    def __init__(self, model, tools, system=""):
+memory = SqliteSaver.from_conn_string(":memory:")
+
+class Agent:
+    def __init__(self, model, tools, checkpointer, system=""):
         self.system = system
         graph = StateGraph(AgentState)
         graph.add_node("llm", self.call_openai)
         graph.add_node("action", self.take_action)
-        graph.add_conditional_edges(
-            "llm",
-            self.exists_action,
-            {True: "action", False: END}
-        )
+        graph.add_conditional_edges("llm", self.exists_action, {True: "action", False: END})
         graph.add_edge("action", "llm")
         graph.set_entry_point("llm")
-        self.graph = graph.compile()
+        self.graph = graph.compile(checkpointer=checkpointer)
         self.tools = {t.name: t for t in tools}
         self.model = model.bind_tools(tools)
-
-    def exists_action(self, state: AgentState):
-        result = state['messages'][-1]
-        return len(result.tool_calls) > 0
 
     def call_openai(self, state: AgentState):
         messages = state['messages']
@@ -157,38 +152,33 @@ class Agent:
         message = self.model.invoke(messages)
         return {'messages': [message]}
 
+    def exists_action(self, state: AgentState):
+        result = state['messages'][-1]
+        return len(result.tool_calls) > 0
+
     def take_action(self, state: AgentState):
         tool_calls = state['messages'][-1].tool_calls
         results = []
         for t in tool_calls:
             print(f"Calling: {t}")
-            if not t['name'] in self.tools:      # check for bad tool name from LLM
-                print("\n ....bad tool name....")
-                result = "bad tool name, retry"  # instruct LLM to retry if bad
-            else:
-                result = self.tools[t['name']].invoke(t['args'])
+            result = self.tools[t['name']].invoke(t['args'])
             results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
         print("Back to the model!")
         return {'messages': results}
-    
+
 prompt = """You are a smart research assistant. Use the search engine to look up information. \
 You are allowed to make multiple calls (either together or in sequence). \
 Only look up information when you are sure of what you want. \
 If you need to look up some information before asking a follow up question, you are allowed to do that!
 """
-
-model = ChatOpenAI(model="gpt-3.5-turbo")  #reduce inference cost
-abot = Agent(model, [tool], system=prompt)
+model = ChatOpenAI(model="gpt-4o")
+abot = Agent(model, [tool], system=prompt, checkpointer=memory)
 
 from IPython.display import Image
 
 Image(abot.graph.get_graph().draw_png())
 
-messages = [HumanMessage(content="What is the weather in sf?")]
-result = abot.graph.invoke({"messages": messages})
-result['messages'][-1].content
-
-#another example
+# example
 query = "Who won the super bowl in 2024? In what state is the winning team headquarters located? \
 What is the GDP of that state? Answer each question." 
 messages = [HumanMessage(content=query)]
@@ -197,3 +187,11 @@ model = ChatOpenAI(model="gpt-4o")  # requires more advanced model
 abot = Agent(model, [tool], system=prompt)
 result = abot.graph.invoke({"messages": messages})
 print(result['messages'][-1].content)
+
+# stream the observations and responses, can keep running this block to see the conversation
+messages = [HumanMessage(content=query)]
+thread = {"configurable": {"thread_id": "1"}}
+for event in abot.graph.stream({"messages": messages}, thread):
+    for v in event.values():
+        print(v['messages'])
+
