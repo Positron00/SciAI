2e7e51baca5e7b9bf1797288f2da9b2b38de3b54
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-12-14 16:44:55 -0500

    add custom reduce_messages() to allow replacing

--- nbs/langGraph_agents.py
+++ nbs/langGraph_agents.py
@@ -124,13 +124,44 @@ query(question)
 tool = TavilySearchResults(max_results=4) #increased number of results
 #print(type(tool))
 #print(tool.name)
-
-class AgentState(TypedDict):
-    messages: Annotated[list[AnyMessage], operator.add]
+    
 
 from langgraph.checkpoint.sqlite import SqliteSaver
 
 memory = SqliteSaver.from_conn_string(":memory:")
+
+from uuid import uuid4
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage
+
+"""
+The 'messages' state key can be annotated with the default 'operator.add' or '+' reducer, 
+which always appends new messages to the end of the existing messages array.
+
+Now, to support replacing existing messages, we annotate the
+'messages' key with a custom reducer function, which replaces
+messages with the same 'id', and appends them otherwise.
+"""
+def reduce_messages(left: list[AnyMessage], right: list[AnyMessage]) -> list[AnyMessage]:
+    # assign ids to messages that don't have them
+    for message in right:
+        if not message.id:
+            message.id = str(uuid4())
+    # merge the new messages with the existing messages
+    merged = left.copy()
+    for message in right:
+        for i, existing in enumerate(merged):
+            # replace any existing messages with the same id
+            if existing.id == message.id:
+                merged[i] = message
+                break
+        else:
+            # append any new messages to the end
+            merged.append(message)
+    return merged
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], reduce_messages]
+#   messages: Annotated[list[AnyMessage], operator.add] # default: always append
 
 class Agent:
     def __init__(self, model, tools, checkpointer, system=""):
